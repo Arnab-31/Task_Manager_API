eb6f0587d2c87dc93edfdaa8969ef20debadd566
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2020-09-24 02:07:46 +0530

    Setting up jest and rearranging the files for writing tests

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true,
-    dbName: 'Task-Manager-API'
+    useUnifiedTopology: true
 },()=>{
     console.log("Database Connected!")
 })
--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const express = require('express');
-require('./db/mongoose');
-const User = require('./models/user');
-const Task = require('./models/task');
-const { count } = require('./models/user');
-const userRouter = require('./routes/user');
-const taskRouter = require('./routes/task');
-
-const app = express();
+const app = require('./app')
 const port = process.env.PORT 
-
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-
 
 app.listen(port, ()=>{
     console.log('Server is up on the port ',  port);
